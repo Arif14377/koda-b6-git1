70b7772f58911fda405f06310349e56810b61479
Arif14377 <arifrahman14377@gmail.com>
2026-01-05 20:55:41 +0700

    feat: melengkapi fitur input

--- utils/input.js
+++ utils/input.js
@@ -4,5 +4,12 @@ import { stdin as input, stdout as output } from 'node:process'
 export const rl = readline.createInterface({input, output})
 
 export async function inputCelcius() {
-    
+    const celcius = Number(await rl.question("\nMasukkan suhu celcius: "))
+
+    if (isNaN(celcius)) {
+        const error = new Error("Inputan suhu bukan angka.")
+        throw error
+    }
+
+    return celcius
 }
