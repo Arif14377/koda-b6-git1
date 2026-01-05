736fbb7bed975e5b0df4ce8dd8f73fbb42465765
Arif14377 <arifrahman14377@gmail.com>
2026-01-05 18:20:37 +0700

    feat: add fitur konversi celcius ke kelvin

---
+++ conversion/celcius-to-kelvin.js
@@ -0,0 +1,9 @@
+export function celciusToKelvin(c) {
+    if (isNaN(c)) {
+        const err = new Error("Parameter bukan angka.")
+        throw err
+        return
+    }
+    const kelvin = c + 273.15
+    return kelvin
+}
