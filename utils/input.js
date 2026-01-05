773a73398a5a03f4155e1aa265d9fe8bc27c88b1
Arif14377 <arifrahman14377@gmail.com>
2026-01-05 20:23:03 +0700

    feat: add fitur input

---
+++ utils/input.js
@@ -0,0 +1,8 @@
+import readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
+
+export const rl = readline.createInterface({input, output})
+
+export async function inputCelcius() {
+    
+}
