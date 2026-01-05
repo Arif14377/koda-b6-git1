afb1c7fbd38c3636c430269e1cc5480327b8af87
Arif14377 <arifrahman14377@gmail.com>
2026-01-05 21:58:58 +0700

    feat: add implementasi fitur input di index.js

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 import { celciusToFahrenheit } from "./conversion/celcius-to-fahrenheit.js"
 import { celciusToKelvin } from "./conversion/celcius-to-kelvin.js"
 import { celciusToReamur } from "./conversion/celcius-to-reamur.js"
+import { inputCelcius, rl } from "./utils/input.js"
 
 
 async function main() {
-    const suhuCelcius = 40
-
-    let hasilKonversiSuhu = 0
-
-    hasilKonversiSuhu = await celciusToFahrenheit(suhuCelcius)
-    console.log("Hasil konversi suhu Celcius ke Fahrenheit: ", hasilKonversiSuhu)
-
-    hasilKonversiSuhu = await celciusToReamur(suhuCelcius)
-    console.log("Hasil konversi suhu Celcius ke Reamur: ", hasilKonversiSuhu)
+    while (true) {
+        try {
+            let hasilKonversiSuhu = 0
     
-    hasilKonversiSuhu = await celciusToKelvin(suhuCelcius)
-    console.log("Hasil konversi suhu Celcius ke Kelvin: ", hasilKonversiSuhu)
+            const suhuCelcius = await inputCelcius()
+        
+            hasilKonversiSuhu = await celciusToFahrenheit(suhuCelcius)
+            console.log("Hasil konversi suhu Celcius ke Fahrenheit: ", hasilKonversiSuhu)
+        
+            hasilKonversiSuhu = await celciusToReamur(suhuCelcius)
+            console.log("Hasil konversi suhu Celcius ke Reamur: ", hasilKonversiSuhu)
+            
+            hasilKonversiSuhu = await celciusToKelvin(suhuCelcius)
+            console.log("Hasil konversi suhu Celcius ke Kelvin: ", hasilKonversiSuhu)
+    
+            rl.close()
+    
+        } catch (error) {
+            console.log(error)
+            continue
+        }
+
+        break
+
+    }
+    
+
 
 
 }
