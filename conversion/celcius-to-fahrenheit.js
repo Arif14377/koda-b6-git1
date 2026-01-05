9d51b8514b139d8c48675a01ea81f30dcbaef0fd
Arif14377 <arifrahman14377@gmail.com>
2026-01-05 17:43:32 +0700

    feat: add fitur konversi celcius ke fahrenheit

---
+++ conversion/celcius-to-fahrenheit.js
@@ -0,0 +1,9 @@
+export function celciusToFahrenheit(c) {
+    if (isNaN(c)) {
+        const err = new Error("Parameter bukan angka.")
+        throw err
+        return
+    }
+    const fahrenheit = (c * 1.8) + 32.0
+    return fahrenheit
+}
